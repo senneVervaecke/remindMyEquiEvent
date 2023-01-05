42a2702f8be86c95b5219d601752490fdaa623e2
senneVervaecke <vervaecke-senne@hotmail.com>
2023-01-05 10:05:56 +0100

    Update competitionWeekSection.tsx
    relayout competition week list

--- components/competitionList/competitionWeekSection.tsx
+++ components/competitionList/competitionWeekSection.tsx
@@ -1,5 +1,6 @@
 import moment from "moment";
 import React from "react";
+import { StyleSheet } from "react-native";
 import { List } from "react-native-paper";
 import { CompetitionSection } from "../../data/types";
 import CompetitionItem from "./competitionItem";
@@ -10,22 +11,26 @@ type CompetitionWeekSectionProps = {
 
 const CompetitionWeekSection = (props: CompetitionWeekSectionProps) => {
     const section = props.section;
-    const [expanded, setExpanded] = React.useState(true);
 
     const weekStartDate = moment().year(section.year).week(section.week).startOf('week');
     const weekEndDate = moment().year(section.year).week(section.week).endOf('week');
-    const title = `Week ${section.week} ${weekStartDate.format('DD/MM')} - ${weekEndDate.format('DD/MM/YYYY')}`;
+    const title = `Week ${section.week}\t\t\t${weekStartDate.format('DD/MM')} - ${weekEndDate.format('DD/MM/YYYY')}`;
 
     return (
-      <List.Accordion
-      title={title}
-      expanded={expanded}
-      onPress={() => setExpanded(!expanded)}>
+      <List.Section>
+        <List.Subheader style={styles.title}>{title}</List.Subheader>
         {section.competitions.map((competition) => (
           <CompetitionItem key={competition.code} competition={competition} />
         ))}
-      </List.Accordion>
+      </List.Section>
     )
   };
+
+  const styles = StyleSheet.create({
+    title: {
+      fontWeight: 'bold',
+      fontSize: 16
+    }
+  });
 
 export default CompetitionWeekSection;
